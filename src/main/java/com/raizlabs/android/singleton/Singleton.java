eccdf460f384dc13eb1e3595796a670748e469d2
Andrew Grosner <andrew.grosner@raizlabs.com>
2014-08-28 16:56:11 -0400

    pushed new version and added persistency to the singleton class

--- src/main/java/com/raizlabs/android/singleton/Singleton.java
+++ src/main/java/com/raizlabs/android/singleton/Singleton.java
@@ -1,5 +1,8 @@
 package com.raizlabs.android.singleton;
 
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+
 /**
  * Author: andrewgrosner
  * Contributors: { }
@@ -19,6 +22,12 @@ public class Singleton<DataClass> {
      * The instance contained in the {@link com.raizlabs.android.singleton.SingletonManager}
      */
     private DataClass mInstance;
+
+    /**
+     * Boolean determines whether we've determined that this class should persist. Note that this class
+     * must implement {@link java.io.Serializable} for it work appropriately
+     */
+    private boolean persists = false;
 
     /**
      * Constructs an empty instance of the singleton. It does not retrieve an instance from the
@@ -42,14 +51,37 @@ public class Singleton<DataClass> {
     }
 
     /**
+     * Returns the singleton and has it persist in disk storage
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public DataClass retrievePersist() {
+        if(mInstance == null) {
+            persists = true;
+            Class type = (Class) ((ParameterizedType)mDataClass.getGenericSuperclass()).getActualTypeArguments()[0];
+            if(!Serializable.class.isAssignableFrom(type)) {
+                throw new RuntimeException("Singleton must implement java.io.Serializable for it to persist");
+            }
+            mInstance = (DataClass) PersistentSingletonManager.getInstance().singleton((Class<Serializable>)mDataClass);
+        }
+        return mInstance;
+    }
+
+    /**
      * Destroys the singleton from the {@link com.raizlabs.android.singleton.SingletonManager} and
-     * releases reference from this class.
+     * releases reference from this class. If this is persistent, it will delete it from the
+     * {@link com.raizlabs.android.singleton.PersistentSingletonManager}
      * @return The item that was removed.
      */
+    @SuppressWarnings("unchecked")
     public DataClass remove() {
         DataClass instance = null;
         if(mInstance != null) {
-            SingletonManager.remove(mDataClass);
+            if(!persists) {
+                SingletonManager.remove(mDataClass);
+            } else {
+                PersistentSingletonManager.getInstance().destroySingleton((Class<Serializable>)mDataClass);
+            }
             instance = mInstance;
             mInstance = null;
         }
